d39dcd6cfe580cd9ad783ebe95e6a16c1e87d4ed
Igor Borzunov <igorborzunov@proton.me>
2023-05-24 20:36:23 +0700

    add tests

--- test/Auction.js
+++ test/Auction.js
@@ -58,23 +58,23 @@ describe("Auction tests", () => {
             return effects.events.filter(e => e.event === "Transfer")[0].args[2];
         };
 
-        placeCard = async (cardId, signer, minPrice = "0") => {
+        placeCard = async (cardId, signer, minPrice = "0", currency = "MCN") => {
             await card.connect(signer).approve(auction.address, cardId);
-            await auction.connect(signer).placeCardToAuction(cardId, minPrice);
+            await auction.connect(signer).placeCardToAuction(cardId, minPrice, currency);
         };
 
-        placeCardToMaticAuction = async (cardId, signer, minPrice = "0") => {
+        placeCardToMaticAuction = async (cardId, signer, minPrice = "0", currency = "MATIC") => {
             await card.connect(signer).approve(maticAuction.address, cardId);
-            await maticAuction.connect(signer).placeCardToAuction(cardId, minPrice);
+            await maticAuction.connect(signer).placeCardToAuction(cardId, minPrice, currency);
         }
 
-        placeBet = async (cardId, signer, price) => {
-            const betTx = await auction.connect(signer).placeBet(cardId, price);
+        placeBet = async (cardId, signer, price, currency = "MCN") => {
+            const betTx = await auction.connect(signer).placeBet(cardId, price, currency);
             await betTx.wait();
         };
 
-        placeBetToMaticAuction = async (cardId, signer, price) => {
-            await maticAuction.connect(signer).placeBet(cardId, price, {value: price});
+        placeBetToMaticAuction = async (cardId, signer, price, currency = "MATIC") => {
+            await maticAuction.connect(signer).placeBet(cardId, price, {value: price}, currency);
         }
 
         takeCard = async (cardId, signer) => {
@@ -93,23 +93,24 @@ describe("Auction tests", () => {
         expect(cardOwner).to.be.equal(bob.address);
 
         await placeCard(cardId, bob);
+        const currency = "MCN";
         const cardOwnerAfterSendingToAuc = await card.ownerOf(cardId);
-        expect(cardOwnerAfterSendingToAuc).to.be.equal(auction.address);
+        expect(cardOwnerAfterSendingToAuc).to.be.equal(auction.address, currency);
 
         // he can not take it immediately
-        await expect(takeCard(cardId, bob)).to.be.revertedWith("TooEarly");
+        await expect(takeCard(cardId, bob, currency)).to.be.revertedWith("TooEarly");
 
         await ethers.provider.send('evm_increaseTime', [48 * 3600 + 5]);
         await ethers.provider.send('evm_mine');
 
         // now he can
-        await takeCard(cardId, bob)
-        const cardOwnerAfterTakingFromAuc = await card.ownerOf(cardId);
+        await takeCard(cardId, bob, currency)
+        const cardOwnerAfterTakingFromAuc = await card.ownerOf(cardId, currency);
         expect(cardOwnerAfterTakingFromAuc).to.be.equal(bob.address);
 
         // And can place it again
         await placeCard(cardId, bob);
-        const cardOwnerAfterSendingToAucSecondTime = await card.ownerOf(cardId);
+        const cardOwnerAfterSendingToAucSecondTime = await card.ownerOf(cardId, currency);
         expect(cardOwnerAfterSendingToAucSecondTime).to.be.equal(auction.address);
     });
 
@@ -144,16 +145,16 @@ describe("Auction tests", () => {
 
     it("Testing Auction with Matic", async () => {
         const cardId = await mintNewCard(bob);
-        await placeCardToMaticAuction(cardId, bob, ethers.utils.parseEther("0.1"));
+        await placeCardToMaticAuction(cardId, bob, ethers.utils.parseEther("0.1"), currency);
 
-        await expect(placeBetToMaticAuction(cardId, alice, ethers.utils.parseEther("0.05"))).to.be.revertedWith("TooFew");
+        await expect(placeBetToMaticAuction(cardId, alice, ethers.utils.parseEther("0.05"), currency)).to.be.revertedWith("TooFew");
         const aliceBalanceBeforeBet = await alice.provider.getBalance(alice.address);
         await placeBetToMaticAuction(cardId, alice, ethers.utils.parseEther("0.12"));
         const aliceBalanceAfterBet = await alice.provider.getBalance(alice.address);
         expect(aliceBalanceAfterBet.sub(aliceBalanceBeforeBet).valueOf()).to.be.closeTo(-120000000000000000n, 1000000000000000n)
 
-        await expect(placeBetToMaticAuction(cardId, carl, ethers.utils.parseEther("0.11"))).to.be.revertedWith("TooFew");
-        await placeBetToMaticAuction(cardId, carl, ethers.utils.parseEther("0.15"));
+        await expect(placeBetToMaticAuction(cardId, carl, ethers.utils.parseEther("0.11"), currency = "MATIC")).to.be.revertedWith("TooFew");
+        await placeBetToMaticAuction(cardId, carl, ethers.utils.parseEther("0.15"), currency = "MATIC");
         const aliceBalanceAfterRefund = await alice.provider.getBalance(alice.address);
         expect(aliceBalanceAfterRefund.sub(aliceBalanceBeforeBet).valueOf()).to.be.closeTo(0n, 2000000000000000n)
 
